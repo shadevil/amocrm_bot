6f34663e0e15526b1ba98ebbd41d79b8a6c2ee4e
shadevil <linkinfan@bk.ru>
2023-04-12 15:36:31 +0500

    FIX authentification

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import time, re
+import time, re, getpass, os
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,35 +6,57 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+global site, login, password
 
-def params(query):
-    with open('params', 'r') as file:
+def read_value(query,file):
+    with open(file, 'r') as file:
         for line in file:
             if query in line:
                 tab_index = line.index('\t')
                 return line[tab_index+1:].strip()
 
+def save_auth_data():
+    if not os.path.exists('auth'):
+        site = input("Введите сайт(без https://): ")
+        set_auth_data('site','https://' + site)
+
+        login = input("Введите логин: ")
+        set_auth_data('login',login)
+
+        password = getpass.getpass("Введите пароль: ")
+        set_auth_data('password',password)
+
 def reg_search(reg,query): return re.match(reg,query)
 
+def set_auth_data(type,data):
+    with open("auth", "a") as f:
+        f.write(type + '\t' + data + "\n")
+
 def start():
+    save_auth_data()
+    site = read_value('site','auth')
+    login = read_value('login','auth')
+    password = read_value('password','auth')
 
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.default_content_setting_values.notifications" : 2}
-    chrome_options.add_experimental_option("prefs",prefs)
-    driver = webdriver.Chrome(executable_path=r'chromedriver',chrome_options=chrome_options)
+    if(site != '' and login != '' and password != ''):
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.default_content_setting_values.notifications" : 2}
+        chrome_options.add_experimental_option("prefs",prefs)
+        driver = webdriver.Chrome(executable_path=r'chromedriver',options=chrome_options)
 
-    driver.get(params('site'))
-    driver.implicitly_wait(0.5)
+        driver.get(site)
+        driver.implicitly_wait(0.5)
 
-    login = driver.find_element(by=By.ID, value="session_end_login")
-    password = driver.find_element(by=By.ID, value="password")
-    submit_button = driver.find_element(by=By.CLASS_NAME, value="auth_form__submit")
+        login_el = driver.find_element(by=By.ID, value="session_end_login")
+        password_el = driver.find_element(by=By.ID, value="password")
+        submit_button = driver.find_element(by=By.CLASS_NAME, value="auth_form__submit")
 
-    login.send_keys(params('login'))
-    password.send_keys(params('pass'))
+        login_el.send_keys(login)
+        password_el.send_keys(password)
 
-    submit_button.click()
-    main(driver)
+        submit_button.click()
+        main(driver)
+    else: print('DATA WAS NOT FOUND')
 
 def search(driver,cards):
     for card in cards:
@@ -43,8 +65,8 @@ def search(driver,cards):
         card.click()
         time.sleep(5)
         context = driver.find_element(by=By.CLASS_NAME, 
-            value=params('context')).get_attribute("innerHTML")
-        if(not reg_search(params('reg'),context) & reg_search(params('city'),context)):
+            value=read_value('context','params')).get_attribute("innerHTML")
+        if(not reg_search(read_value('reg','params'),context) & reg_search(read_value('city','params'),context)):
         #     accept = driver.find_element(by=By.ID, value="card_unsorted_accept")
             time.sleep(5)
             print('MATCH')
@@ -56,7 +78,7 @@ def search(driver,cards):
 
 def main(driver):
     wait = WebDriverWait(driver, 10)
-    leads = wait.until(EC.element_to_be_clickable((By.XPATH, params('leads'))))
+    leads = wait.until(EC.element_to_be_clickable((By.XPATH, read_value('leads','params'))))
     leads.click()
 
 
