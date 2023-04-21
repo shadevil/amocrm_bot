4ced763f2c9730324e2b7ae22a456ec5d86e7c77
shadevil <linkinfan@bk.ru>
2023-04-22 02:48:12 +0500

    delete chromeload? refactoring and change to russian comments

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import time, re, getpass, os
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -17,7 +19,7 @@ xpath_dict = {
         "left-menu-overlay",
 
     "datetime":
-        "//div/div[@class='feed-note__date']",
+        '//div[@class="feed-note__date"][1]',
 
     "purchases_main":
         "//div[1]/div/div/div/div/a[@class='pipeline_leads__title-text h-text-overflow js-navigate-link']",
@@ -46,16 +48,15 @@ def match_log(matches):
         for match in matches:
             write_log("Match found: " + match)
 
-def auth_data():
-    if not os.path.exists('auth'):
-        site = input("Введите сайт(без https://): ")
-        set_auth_data('site','https://' + site)
+def auth_data():   
+    site = input("Введите сайт(без https://): ")
+    set_auth_data('site','https://' + site)
 
-        login = input("Введите логин: ")
-        set_auth_data('login',login)
+    login = input("Введите логин: ")
+    set_auth_data('login',login)
 
-        password = getpass.getpass("Введите пароль: ")
-        set_auth_data('password',password)
+    password = getpass.getpass("Введите пароль: ")
+    set_auth_data('password',password)
 
 def get_auth_data(query):
     file = 'auth'
@@ -70,7 +71,7 @@ def set_auth_data(type,data):
         f.write(type + '\t' + data + "\n")
 
 def write_log(message):
-    date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    date_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     data = date_time + ' ' + message
     print(data)
     with open("log", "a") as f:
@@ -80,17 +81,18 @@ def reg_search(reg,query): return re.findall(reg, query)
 
 def start():
     try:
-        auth_data()
+        if not os.path.exists('auth'): auth_data()
         site = get_auth_data('site')
         login = get_auth_data('login')
         password = get_auth_data('password')
-
+        
         if(site != '' and login != '' and password != ''):
-            chrome_options = webdriver.ChromeOptions()
+            options = webdriver.ChromeOptions()
             prefs = {"profile.default_content_setting_values.notifications" : 2}
-            chrome_options.add_experimental_option("prefs",prefs)
-            # изменить, если запуск не на windows
-            driver = webdriver.Chrome(executable_path=r'chromedriver',options=chrome_options)
+            options.add_experimental_option("prefs",prefs)
+            # options.headless = True
+            # изменить, если запуск не на windows               service=Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(executable_path=r'chromedriver',chrome_options=options)
 
             driver.get(site)
             driver.implicitly_wait(0.5)
@@ -102,46 +104,50 @@ def start():
             login_el.send_keys(login)
             password_el.send_keys(password)
 
+            
             submit_button.click()
             overlay_exist = True
             move_to_purchases(driver,overlay_exist)
-        else: write_log('DATA WAS NOT FOUND')
+        else: write_log('Данные не найдены')
     except WebDriverException as ex:
         write_log(ex.msg)
         start()
 
 def datetime_result(driver):
-    dt = driver.find_element(By.XPATH, xpath_dict['datetime']).get_attribute('innerHTML')
-    write_log('RECEIVED DATETIME ' + dt)
+    wait = WebDriverWait(driver,10)
+    dt = wait.until(EC.presence_of_element_located((By.XPATH, xpath_dict['datetime']))).get_attribute('innerHTML')
     if(len(dt) != 16):
         now = datetime.now()
         yesterday = datetime.now() - timedelta(days=1)
         today = re.findall("сегодня", dt, flags=re.IGNORECASE)
         time_str = dt.split(" ")[1]
-        time_dt = datetime.strptime(time_str, "%H:%M")
-        return datetime(now.year, now.month, now.day, time_dt.hour, time_dt.minute) if today else datetime(yesterday.year, yesterday.month, yesterday.day, time_dt.hour, time_dt.minute)
-    if(dt is None): return None
-    else: return datetime.strptime(dt, '%d.%m.%Y %H:%M').replace(second=0, microsecond=0)
+        time_dt = datetime.strptime(time_str, '%H:%M:%S.%f')
+        if today:
+            return datetime(now.year, now.month, now.day, time_dt.hour, time_dt.minute, time_dt.second, time_dt.microsecond)
+        else:
+            return datetime(yesterday.year, yesterday.month, yesterday.day, time_dt.hour, time_dt.minute, time_dt.second, time_dt.fold)
+    elif(dt is None): return None
+    else: return datetime.strptime(dt, '%d.%m.%Y %H:%M:%S.%f')
 
 def search(driver,type):
     wait = WebDriverWait(driver, 10)
     # FIXME: accept button
-    write_log("====================================")
     cards = driver.find_elements(by=By.XPATH, value=xpath_dict['purchases_'+type])
-    write_log('LEN CARDS ' + str(len(cards)))      
+    write_log('Всего карточек ' + str(len(cards)))      
     write_log(type.upper())
     for card in cards:                    
         wait.until(EC.element_to_be_clickable(card)).click()
+        write_log("====================================")
         time.sleep(1)
         wait = WebDriverWait(driver, 10)
         result = datetime_result(driver)
         if(result is not None): diff = datetime.now() - result
-        write_log("DATETIME DIFF " + str(diff))
+        write_log("Прошло времени " + str(diff))
         back = wait.until(EC.element_to_be_clickable((By.XPATH, xpath_dict['back'])))
-        if(result is not None and diff >= timedelta(minutes=15) and diff < timedelta(days=3)):
+        if(result is not None and diff >= timedelta(minutes=15) and diff >= timedelta(milliseconds=15) and diff < timedelta(days=3)):
             time.sleep(3)
             elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, xpath_dict['content_'+type])))
-            write_log("LEN ELEMENTS " + str(len(elements)))                    
+            write_log("Количество строк в описании " + str(len(elements)))                    
             for x in elements:
                 #Uncommit this for test
                 # write_log(str(x.get_attribute('innerHTML')))
@@ -153,8 +159,8 @@ def search(driver,type):
                 
                 if(not reg and city):
                     # accept = driver.find_element(by=By.ID, value="card_unsorted_accept")
-                    write_log('MATCH')
-                    # accept.click()      
+                    write_log('Найдено совпадение!')
+                    # accept.click()     
         back.click()
 
 def is_mouse_on_element(driver, xpath):
@@ -169,8 +175,7 @@ def is_mouse_on_element(driver, xpath):
     
 
 def move_to_purchases(driver,overlay_exist):
-    wait = WebDriverWait(driver, 10)
-    write_log("OVERLAY_EXIST " + str(overlay_exist))    
+    wait = WebDriverWait(driver, 10)  
     try:        
         if(overlay_exist):
             wait.until(EC.element_to_be_clickable((By.XPATH, xpath_dict['leads']))).click()
